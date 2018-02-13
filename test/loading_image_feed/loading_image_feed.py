3e114f37c60bb88a2d7a06b7c3f046974890a7b9
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2018-02-13 23:47:09 +0530

    loading_images

---
+++ test/loading_image_feed/loading_image_feed.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import Tkinter
+import matplotlib.pyplot as plt
+
+img = cv2.imread('watch.jpg', cv2.IMREAD_GRAYSCALE)
+#IMREAD_COLOUR -> NUMBER 1
+#IMREAD_UNCHANGED = -1, GRAY_SCALE = 2
+
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# plt.imshow(img, cmap='gray', interpolation='bicubic')
+# plt.plot([50, 100], [80, 100], 'c', linewidth=5)
+# plt.show()
+
+#saving the image
+cv2.imwrite('watchgray.png', img);
